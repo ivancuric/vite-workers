dd85034629f4e90db49125b989e39131912b03ce
Ivan Čurić <ivan.curic@gmail.com>
2022-05-24 15:36:29 +0200

    Add callback

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
+import { remoteFunction } from './worker';
+
 // Create Worker
-const instance = new ComlinkWorker<typeof import('./worker')>(
+const workerInstance = new ComlinkWorker<typeof import('./worker')>(
   new URL('./worker', import.meta.url),
 );
-const result = await instance.add(2, 3);
+const result = await workerInstance.add(2, 3);
+
+export const callback = (string: string) => console.log(string);
 
 console.log(result);
+
+await remoteFunction(callback);
+
+export {}
--- src/worker.ts
+++ src/worker.ts
@@ -1,2 +1,8 @@
 // worker.ts
 export const add = (a: number, b: number) => a + b;
+
+type Callback = typeof import('./main').callback
+
+export async function remoteFunction(callback: Callback) {
+  callback("A string from a worker");
+}
